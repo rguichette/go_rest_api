0301a158aa1aee16affbaee85784ae9c72cffac4
ralph <ralphgui@gmail.com>
2022-12-17 16:33:52 -0500

    trying to connect db layer

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rguichette/go_rest_api/internal/db"
+)
 
 //Run - is going to be responsible for the instantiation and startup of our go application
 func Run() error {
 	fmt.Println("Starting up our application")
+	db, err := db.NewDatabase()
+
+	if err != nil {
+		fmt.Println("Failed to connect to the Database")
+		return err
+	}
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate database")
+		return err
+	}
+
 	return nil
 }
 
---
+++ migrations/0001_create_comments_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS Comments (
+    ID uuid,
+    Slug text,
+    Author text,
+    Body text
+);
